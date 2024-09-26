c7ee88dcc7c1642051b09c56150ce6aca5481532
kmk2024 <kmk2024@163.com>
2024-09-27 00:02:54 +0800

    Gurobi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    // MQXAQueueConnectionFactory
     public static void main(String[] args) {
         // Press Ctrl+1 with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
